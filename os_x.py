e7e5c321cc0b254f1752f5a17a8467ee32ae66dc
Jesse Aldridge <JesseAldridge@gmail.com>
2015-09-02 14:12:57 -0700

    cache screen size

--- os_x.py
+++ os_x.py
@@ -2,11 +2,16 @@ import subprocess, os, time, re
 
 from PIL import Image
 
+class g:
+  cached_size = None
+
 def get_screen_size():
-  proc = subprocess.Popen(['system_profiler', 'SPDisplaysDataType'], stdout=subprocess.PIPE)
-  result = proc.communicate()[0]
-  dims = re.search('Resolution.*', result).group().split(':')[-1].split('x')
-  return [int(x) for x in dims]
+  if not g.cached_size:
+    proc = subprocess.Popen(['system_profiler', 'SPDisplaysDataType'], stdout=subprocess.PIPE)
+    result = proc.communicate()[0]
+    dims = re.search('Resolution.*', result).group().split(':')[-1].split('x')
+    g.cached_size = [int(x) for x in dims]
+  return g.cached_size
 
 def take_screenshot():
   ' Return an Image of the screen. '
@@ -18,7 +23,8 @@ def take_screenshot():
   return img
 
 if __name__ == '__main__':
-  print get_screen_size()
-  # for i in range(5):
-  #   print take_screenshot()
-  #   time.sleep(1)
+  for i in range(10):
+    print get_screen_size()
+  for i in range(2):
+    print take_screenshot()
+    time.sleep(1)
