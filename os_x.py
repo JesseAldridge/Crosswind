ac254ec021b4bc0dd326e0df59720bdfbee75ac7
Jesse Aldridge <JesseAldridge@gmail.com>
2015-09-02 12:33:27 -0700

    do less

--- os_x.py
+++ os_x.py
@@ -1,13 +1,18 @@
+import AppKit
 
-def current_file():
-  # Pull current file from the text editor.
-  for win in all_windows():
-    if win32gui.IsWindowVisible(win):
-      title = win32gui.GetWindowText(win)
-      for title_str in [' - Notepad++', ' - Sublime Text']:
-        if title_str in title:
-          path = title.split(title_str)[0]
-          if path.startswith(my_paths.goggles):
-            db = DirDb(join(expanduser('~'), 'goggles'))
-            return db.get(basename(path) + '_dir', 'full_path')
-          return path
+def get_screen_size():
+  return max([(screen.frame().size.width, screen.frame().size.height) 
+    for screen in AppKit.NSScreen.screens()])
+
+def take_screenshot():
+  ' Return an Image of the screen. '
+
+  cmd_line = 'screencapture -t png -x temp_img.png'
+  subprocess.Popen(cmd_line.split()).communicate()
+  img = Image.open('temp_img.png')
+  os.remove('temp_img.png')
+  return img
+
+if __name__ == '__main__':
+  print get_screen_size()
+  
