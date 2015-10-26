dffe8d947787d3854ff80faab03c76a1fb507454
Jesse Aldridge <JesseAldridge@gmail.com>
2015-10-25 17:58:07 -0700

    write screen size to disk in-case system_profiler fails

--- os_x.py
+++ os_x.py
@@ -3,18 +3,28 @@ import subprocess, os, time, re, tempfile
 class g:
   cached_size = None
 
-def get_screen_size():
+def get_screen_size(write_to_disk=True):
   if not g.cached_size:
+    g.cached_size = [0, 0]
     proc = subprocess.Popen(['system_profiler', 'SPDisplaysDataType'], stdout=subprocess.PIPE)
     result = proc.communicate()[0]
-    dims = re.search('Resolution.*', result).group().split(':')[-1].split('x')
-    g.cached_size = [int(x) for x in dims]
+    match = re.search('Resolution.*', result)
+    dims = match.group().split(':')[-1].split('x')
+    if match:
+      g.cached_size = [int(x) for x in dims]
+    # (I think system_profiler can fail around shutdown or startup, so fallback to file.)
+    elif write_to_disk:
+      with open(os.path.expanduser('~/.screen_size')) as f:
+        g.cached_size = f.read().split()
+    if write_to_disk:
+      with open(os.path.expanduser('~/.screen_size'), 'w') as f:
+        f.write(' '.join([str(x) for x in g.cached_size]))
   return g.cached_size
 
 def take_screenshot():
   ' Return an Image of the screen. '
 
-  tf = tempfile.NamedTemporaryFile()
+  tf = tempfile.NamedTemporaryFile(delete=False)
   print 'name:', tf.name
   cmd_line = 'screencapture -t png -x {}'.format(tf.name)
   subprocess.Popen(cmd_line.split()).communicate()
